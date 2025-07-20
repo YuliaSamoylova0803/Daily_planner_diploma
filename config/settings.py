76e6e96fe433475c9cd588049f30610b36c750ba
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-20 08:53:20 +0500

    update setting in config

--- config/settings.py
+++ config/settings.py
@@ -127,15 +127,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Настройки для медиа файлов (загружаемые пользователями)
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
