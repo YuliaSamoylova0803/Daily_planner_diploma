fb3d74b4e07dcaeaa20aca4297a134a4fe1a9121
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-21 21:39:27 +0500

    register model DefectImage in admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, DefectImage
 
 # Register your models here.
 admin.site.register(Note)
+
+@admin.register(DefectImage)
+class DefectImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'note', 'defect_number', 'description')
+    list_filter = ('note', 'defect_number')
+    search_fields = ('description', 'note__title')
