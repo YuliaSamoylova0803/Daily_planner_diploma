f6b8f09a8ddcdd1147ace94640e7eb721cf8196b
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-22 14:53:24 +0500

    update views in app notes

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
 import logging
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
