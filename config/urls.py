adb1aececfffedefa715a1fb3eef2e9cde3e1346
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-22 14:54:08 +0500

    update urls in config

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    path("users/", include("users.urls", namespace="users")),
     path("", include("notes.urls", namespace="notes"))
 ]
 if settings.DEBUG:
