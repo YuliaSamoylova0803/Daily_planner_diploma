0706e79db7e988faa73c16c8c45602d0396ebe2c
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-22 22:41:19 +0500

    update urls in app notes

--- notes/urls.py
+++ notes/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from notes.apps import NotesConfig
-from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDetailView, NoteDeleteView, download_document
+from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDetailView, NoteDeleteView, download_document, \
+    BaseView
 
 app_name = NotesConfig.name  # или можно просто указать app_name = 'notes'
 
 urlpatterns = [
+    path("", BaseView.as_view(), name="base"),
     path("create/", NoteCreateView.as_view(), name="create"),
     path("<int:pk>/delete/", NoteDeleteView.as_view(), name="delete"),
     path("<int:pk>/update/", NoteUpdateView.as_view(), name="update"),
     path("<int:pk>/download/", download_document, name="download"),
     path("<int:pk>/", NoteDetailView.as_view(), name="detail"),
-    path("", NoteListView.as_view(), name="list"),
+    path("list/", NoteListView.as_view(), name="list"),
 ]
