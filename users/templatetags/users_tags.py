f992e25a0b819598440cdfc1374923461d229359
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-22 22:45:10 +0500

    add tags in app users

---
+++ users/templatetags/users_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.filter(name='media_filter')  # Явно задаем имя фильтра
+def media_filter(path):
+    if path:
+        return f"{settings.MEDIA_URL}{path}"
+    return "#"
+
+@register.filter(name='filename')  # Явно задаем имя фильтра
+def filename(value):
+    return value.split("/")[-1] if value else ""
