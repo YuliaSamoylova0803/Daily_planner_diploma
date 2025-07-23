2e2add8d4fe7021e8af23b1d449de0a9a4a5f362
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-23 18:58:07 +0500

    update urls in app notes

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from notes.apps import NotesConfig
-from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDetailView, NoteDeleteView, download_document, \
-    BaseView, generate_document
+from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDetailView, NoteDeleteView, BaseView, \
+    AddImagesView, download_document
 
 app_name = NotesConfig.name  # или можно просто указать app_name = 'notes'
 
@@ -10,8 +10,14 @@ urlpatterns = [
     path("create/", NoteCreateView.as_view(), name="create"),
     path("<int:pk>/delete/", NoteDeleteView.as_view(), name="delete"),
     path("<int:pk>/update/", NoteUpdateView.as_view(), name="update"),
-    path("<int:pk>/download/", download_document, name="download"),
     path("<int:pk>/", NoteDetailView.as_view(), name="detail"),
     path("list/", NoteListView.as_view(), name="list"),
-    path('<int:pk>/generate/', generate_document, name='generate_document'),
+    path('<int:pk>/add-images/', AddImagesView.as_view(), name='add_images'),
+    path('<int:pk>/download/', download_document, name='download_document'),
+
+# Специальные URL для создания конкретных типов записей
+    path('personal/create/', NoteCreateView.as_view(), name='create_personal'),
+    path('work/create/', NoteCreateView.as_view(), name='create_work'),
+    path('defect/create/', NoteCreateView.as_view(), name='create_defect'),
+    path('statement/create/', NoteCreateView.as_view(), name='create_statement'),
 ]
