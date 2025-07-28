36aa86376d5b1fd62abfd65a7645208769b32423
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-28 14:01:54 +0500

    update urls in config

--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from ninja import NinjaAPI
+from notes.api import api as notes_api
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", notes_api.urls),
+
     path("users/", include("users.urls", namespace="users")),
     path("", include("notes.urls", namespace="notes")),
+
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
